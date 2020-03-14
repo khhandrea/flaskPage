5e91bc15fa589ea9a363e51666fbf3e56300d9b0
khhandrea <44332152+khhandrea@users.noreply.github.com>
2020-03-14 21:16:52 +0900

    플라스크 hello world

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+content = "hello guys!"
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/<name>')
+def home(name):
+    return render_template('home.html', content=name)
+
+
+if __name__ == '__main__':
+    app.run()
